81a1c4ec91ee102478e494081c95b9f7fd979480
Ethan <EETfitness@gmail.com>
2018-11-28 15:34:40 -0800

    adding assignment 1.5.js

---
+++ week-1/assignment-1.5.js
@@ -0,0 +1,89 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Ethan Townsend
+; Date:   28 November 2018
+; Modified By: Ethan Townsend
+; Description: This program demonstrates the
+;   use of JavaScript types, values, and 
+;   and variables in an application.
+;===========================================
+*/ 
+
+/*
+    The code below builds a properly formatted
+    header and must be included in all applications
+    you write. In subsequent week's we will build this
+    functionality into a function and place it in a separate file.
+*/
+
+var myFirstName = "Albert";
+var myLastName = "Einstein";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+
+var firstName = "Isaac";
+var lastName = "Newton";
+var address = "Somewhere in England"
+var pay = "$1,000,000"
+var hireDate = new Date(1643,1,4)
+
+var programHeader1 = "\n" + "\nFirst Name:" + " " + firstName + "\nLast Name:" + " " + lastName + "\nAddress:" + " " + address + "\nPay Rate:" + " " + pay + "\nHire Date:" + " " + hireDate;
+
+console.log(programHeader1);
+console.log("\n")
+
+var firstName = "Benjamin";
+var lastName = "Franklin";
+var address = "Milk Street, Boston, MA"
+var pay = "$2,000,000"
+var hireDate = new Date(1706,1,17)
+
+var programHeader2 = "\n" + "\nFirst Name:" + " " + firstName + "\nLast Name:" + " " + lastName + "\nAddress:" + " " + address + "\nPay Rate:" + " " + pay + "\nHire Date:" + " " + hireDate;
+
+console.log(programHeader2);
+console.log("\n")
+
+var firstName = "Thomas";
+var lastName = "Edison";
+var address = "Milan, OH"
+var pay = "$3,000,000"
+var hireDate = new Date(1847,2,11)
+
+var programHeader3 = "\n" + "\nFirst Name:" + " " + firstName + "\nLast Name:" + " " + lastName + "\nAddress:" + " " + address + "\nPay Rate:" + " " + pay + "\nHire Date:" + " " + hireDate;
+
+console.log(programHeader3);
+console.log("\n")
+
+var firstName = "Albert";
+var lastName = "Einstein";
+var address = "Ulm, Germany"
+var pay = "$4,000,000"
+var hireDate = new Date(1879,3,14)
+
+var programHeader4 = "\n" + "\nFirst Name:" + " " + firstName + "\nLast Name:" + " " + lastName + "\nAddress:" + " " + address + "\nPay Rate:" + " " + pay + "\nHire Date:" + " " + hireDate;
+
+console.log(programHeader4);
+console.log("\n")
+
+var firstName = "John";
+var lastName = "Locke";
+var address = "Wrington, United Kingdom"
+var pay = "$1,000,000,000,000"
+var hireDate = new Date(1632,8,29)
+
+var programHeader5 = "\n" + "\nFirst Name:" + " " + firstName + "\nLast Name:" + " " + lastName + "\nAddress:" + " " + address + "\nPay Rate:" + " " + pay + "\nHire Date:" + " " + hireDate;
+
+
+console.log(programHeader5);
+console.log("\n")
+
+// end of program 
