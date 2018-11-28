6b229c51e57c4ff7b209305d4687833b063a7b8f
Ethan <EETfitness@gmail.com>
2018-11-28 12:42:18 -0800

    Adding assignment 1.3.js

---
+++ week-1/hello-world.js
@@ -0,0 +1,13 @@
+console.log("Hello World, my name is Ethan Townsend");
+
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Ethan Townsend
+; Date: 28 November 2018
+; Modified By: Ethan Townsend
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
