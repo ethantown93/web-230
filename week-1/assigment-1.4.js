2593a0388c61359582edb4464b9f9bc764ec3054
Ethan <EETfitness@gmail.com>
2018-11-28 13:17:30 -0800

    Adding assignment 1.4.js

---
+++ week-1/assigment-1.4.js
@@ -0,0 +1,29 @@
+// This is my second JavaScript Code
+
+var firstName = "Ethan";
+var lastName = "Townsend";
+var age = "25";
+var street = "Willis Road";
+var city = "El Cajon";
+var state = "California";
+var zipCode = "92020";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Ethan Townsend
+; Date: 28 November 2018
+; Modified By: Ethan Townsend
+; Description: This program demonstrates the
+; use of JavaScript types, values, and
+; and variables in an application.
+;===========================================
+*/ 
