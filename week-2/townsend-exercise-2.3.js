baac03fa76dbf7d46fe647f42ebec473a4dd638b
Ethan <EETfitness@gmail.com>
2018-12-05 15:59:17 -0800

    assignment 2.3

---
+++ week-2/townsend-exercise-2.3.js
@@ -0,0 +1,37 @@
+var header = require('./townsend.js');
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 2.3
+  Today's Date
+
+  Hello FirstName LastName!
+
+  Hint: Use your personal header display function and
+  refer to page 178 for implementing function properties
+*/
+
+//function properties
+myName.ethan = "Ethan";
+lastName.townsend = "Townsend!"
+greeting.hello = "Hello"
+
+//function
+function myName() {
+    return myName.ethan;
+}
+
+function lastName() {
+    return lastName.townsend;
+}
+
+function greeting() {
+    return greeting.hello
+}
+
+//output
+console.log('\n');
+console.log(greeting() + ' ' + myName() + ' ' + lastName());
+
+//end program
