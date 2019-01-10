6cb572209b3e8113e208bbb0d7d55fce3de4878f
Ethan <EETfitness@gmail.com>
2019-01-10 11:02:11 -0800

    adding assignment 5.2

---
+++ week-5/townsend-assignment-5.2.js
@@ -0,0 +1,21 @@
+// Header
+var header = require('../week-2/townsend.js');
+console.log(header.display("Ethan", "Townsend", "assignment 5.2"));
+console.log('\n');
+
+/*
+============================================
+; Title:  townsend-assignment-5.2.js
+; Author: Professor Krasso
+; Date:   09 January 2019
+; Modified By: Ethan Townsend
+; Description:  Advanced Arrays
+;===========================================
+*/
+
+var foods = ['Pizza', 'Ice cream', 'Cheeseburger', 'Sandwhich', 'Donut'];
+
+foods.forEach(function(food){
+    console.log(food);
+});
+
