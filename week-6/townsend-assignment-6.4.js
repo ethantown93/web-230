e7b835cdf788ce3372b79dad19c86eb4f9fb58a2
Ethan <EETfitness@gmail.com>
2019-01-16 12:35:07 -0800

    adding assignment 6.4

---
+++ week-6/townsend-assignment-6.4.js
@@ -0,0 +1,32 @@
+// Header
+var header = require('../week-2/townsend.js');
+console.log(header.display("Ethan", "Townsend", "assignment 6.4"));
+console.log('\n');
+
+/*
+============================================
+; Title:  townsend-assignment-6.5.js
+; Author: Professor Krasso
+; Date:   16 January 2019
+; Modified By: Ethan Townsend
+; Description:  Nested Objects
+;===========================================
+*/
+
+const ticketing = {
+    id: 'CA drivers license',
+    name: 'speeing ticket',
+    date: new Date().toLocaleDateString(),
+    priority: 'misdemeanor',
+
+    person: {
+        id1: 'CA drivers license',
+        firstName: 'Michael ',
+        lastName: 'Jackson',
+        job: 'Singer/Performer',
+    }
+
+}; 
+
+console.log( ticketing.person.firstName + ticketing.person.lastName + '(the ' + ticketing.person.job + ')' + ' has received a '+ ticketing.priority + ' ' + ticketing.name + ' on ' + ticketing.date );
+console.log('The ticket is a ' + ticketing['priority'] + ' so his ' + ticketing['id'] + ' will not be suspended.');
